a6b5267fb42fc9a9a1fc8dfd0f03382be0ca895e
Evgeny Grossman <40717805+BeschAmon@users.noreply.github.com>
2026-02-09 01:29:55 +0300

    Align instruments list columns

---
+++ Views/Panels/InstrumentsPanel.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace TradingTerminal.Views.Panels
+{
+    public partial class InstrumentsPanel : UserControl
+    {
+        public InstrumentsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
